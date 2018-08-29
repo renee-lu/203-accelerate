1902fbeab6de855965d164e1afcf0170b154dc47
Renee Ramirez <rera0487@gmail.com>
2018-08-29 11:03:39 -0400

    changes to accelerate theme

--- wp-content/themes/accelerate-theme/footer.php
+++ wp-content/themes/accelerate-theme/footer.php
@@ -20,7 +20,8 @@
 				</div>
 
 				<nav class="social-media-navigation" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
+					
+					<?php echo do_shortcode('[aps-social id="1"]')?>
 				</nav>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
